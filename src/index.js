bc7285c628aa35e3ad8edfdd3a853ccc9e381d10
MarinaBaidyk <marina.baidyk@catenamedia.com>
2021-11-03 16:08:16 +0200

    added index.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { StrictMode } from "react";
 
 import { App } from "./components/App";
+import './index.css';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
